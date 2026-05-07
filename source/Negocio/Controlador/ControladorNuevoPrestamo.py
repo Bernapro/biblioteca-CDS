9afb474457cadcde3be6f3b7fb5355f5ebff9ed6
joseaguilar4443 <ja410837@gmail.com>
2026-05-06 21:46:17 -0600

    implementacion de  validaciones visuales integradas en nuevo prestamos y login

--- source/Negocio/Controlador/ControladorNuevoPrestamo.py
+++ source/Negocio/Controlador/ControladorNuevoPrestamo.py
@@ -5,8 +5,8 @@ from Negocio.Modelo.Usuario import Usuario
 from datetime import datetime
 from Negocio.Utilidades.Validador import Validador
 
-
 class ControladorNuevoPrestamo:
+    # Inicializa controlador
     def __init__(self, pantalla, endEjemplares: BibliotecaClientInterface, repositorio: RepositorioImpl, endPrestamo: BibliotecaClientInterface):
         self.__pantalla = pantalla
         self.__endEjemplares = endEjemplares
@@ -14,27 +14,24 @@ class ControladorNuevoPrestamo:
         self.__usuario = None
         self.__endPrestamo = endPrestamo
 
-
-
-
+    # Escucha eventos de botones
     def listener(self,e):
         id = e.control.data
         if id:
             if id == "btn_buscar":
                 self.buscarLibroPorNoAdquisicion()
 
-
+    # Busca alumno en BD
     def buscar_alumno(self, e):
         self.__usuario = Usuario()
-        matricula_buscar = ""
         matricula_buscar = self.__pantalla.txt_matricula.value
+        
+        self.__pantalla.ocultar_alerta() 
+        
         if len(matricula_buscar) > 6:
             usr = self.__repo.buscar_usuario_por_identificador(matricula_buscar)
-            print(usr)
             if usr:
                 self.__usuario.set_columns(usr)
-
-                # Actualizamos la tarjeta dinámicamente con los datos reales
                 self.__pantalla.card_alumno.bgcolor = "surfaceVariant"
                 self.__pantalla.card_alumno.border_radius = 12
                 self.__pantalla.card_alumno.padding = 15
@@ -51,25 +48,35 @@ class ControladorNuevoPrestamo:
                         border_radius=15
                     )
                 ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN)
-            
+            else:
+                self.__usuario = None 
+                self.__pantalla.marcar_error_borde(self.__pantalla.txt_matricula)
+                self.__pantalla.card_alumno.bgcolor = ft.Colors.TRANSPARENT
+                self.__pantalla.card_alumno.content = ft.Text("Usuario no encontrado en la base de datos.", color="red") 
         else:
-            # Lógica en caso de que no se encuentre el alumno o el campo esté vacío
+            self.__usuario = None
+            self.__pantalla.marcar_error_borde(self.__pantalla.txt_matricula)
             self.__pantalla.card_alumno.bgcolor = ft.Colors.TRANSPARENT
-            self.__pantalla.card_alumno.content = ft.Text("Por favor, ingresa una matrícula válida.", color="red")
+            self.__pantalla.card_alumno.content = ft.Text("Por favor, ingresa una matrícula válida.", color="red") 
             
         self.__pantalla.update()
 
+    # Busca libro por numero de adquisicion
     def buscarLibroPorNoAdquisicion(self):
         noAdquisicion = self.__pantalla.txt_adquisicion.value
+        self.__pantalla.ocultar_alerta()
+        
         ejem = self.__endEjemplares.get(noAdquisicion)
         if ejem:
             args = ejem.getBody()
             self.__pantalla.libros_cache = [(args["id"], args["libro"].getTitulo(), " ".join(args["libro"].getAutores()), args["noAdquisicion"], args["disponible"])]
             self.__pantalla.actualizar_lista()
+        else:
+            self.__pantalla.marcar_error_borde(self.__pantalla.txt_adquisicion)
+            self.__pantalla.mostrar_alerta("Libro no encontrado.")
 
-
+    # Retorna datos de prueba
     def obtener_libros_prueba(self):
-        # Más adelante esto será una consulta a la Base de Datos
         return [
             ("El principito", "Antoine de Saint-Exupéry", "ADQ-001245"),
             ("1984", "George Orwell", "ADQ-001246"),
@@ -78,12 +85,26 @@ class ControladorNuevoPrestamo:
             ("Rayuela", "Julio Cortázar", "ADQ-001249")
         ]
 
+    # Valida y guarda el prestamo
     def registrar_prestamo(self, e):
-        if self.__usuario:
-            ejemplaresIds = [tupla[0] for tupla in self.__pantalla.libros_seleccionados.values()]
-            fecha_limite = self.__pantalla.txt_fecha_limite.value
-            fecha_objeto = datetime.strptime(fecha_limite, "%d/%b/%Y")
-            fecha_sql = fecha_objeto.strftime("%Y-%m-%d")
-            args = {"usuario": self.__usuario.getIdentificador(), "fechaLimite": fecha_sql, "ejemplaresIds": ejemplaresIds}
-            self.__endPrestamo.post(args)
-            self.__pantalla.limpiar_pantalla()
+        self.__pantalla.ocultar_alerta()
+
+        if not self.__usuario or not self.__usuario.getIdentificador():
+            self.__pantalla.marcar_error_borde(self.__pantalla.txt_matricula)
+            self.__pantalla.mostrar_alerta("Por favor, busca y verifica primero la matrícula del alumno.")
+            return
+
+        if not self.__pantalla.libros_seleccionados:
+            self.__pantalla.mostrar_alerta("Debes seleccionar al menos un libro para el préstamo.")
+            return
+            
+        ejemplaresIds = [tupla[0] for tupla in self.__pantalla.libros_seleccionados.values()]
+        fecha_limite = self.__pantalla.txt_fecha_limite.value
+        fecha_objeto = datetime.strptime(fecha_limite, "%d/%b/%Y")
+        fecha_sql = fecha_objeto.strftime("%Y-%m-%d")
+        
+        args = {"usuario": self.__usuario.getIdentificador(), "fechaLimite": fecha_sql, "ejemplaresIds": ejemplaresIds}
+        self.__endPrestamo.post(args)
+        
+        self.__pantalla.limpiar_pantalla()
+        self.__usuario = None
